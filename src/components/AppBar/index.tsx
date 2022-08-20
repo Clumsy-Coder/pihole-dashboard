07ebd6ac77e664cac7f174019952da25de6b3380
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-08-20 13:30:13 -0600

    feat(component): add 'AppBar' component
      ## what
      - add AppBar component
      - display site name
        - clicking on it will navigate to the home page
      - render drawer
      - render permanent drawer if the screen is large enough
      - render temporary drawer if rendering on a tablet or phone
      - render menu button if rendering on a tablet or phone
      - open drawer when clicking on menu icon
      - close drawer on a tablet or phone when clicking outside of drawer
      - close drawer on a tablet or phone when clicking a drawer list item
    
      ## how
      - Used Material-UI AppBar to render AppBar
      - Used custom 'Drawer' component to render a temporary and permanent Drawer
    
      ## why
      - a good way to render page navigation options depending on the screen
        size
    
      ## where
      - code placed in `./src/component/AppBar` folder
    
      ## usage
    
      ## TODO
      - need to offset the drawer to the right when rendering a permanent Drawer

---
+++ src/components/AppBar/index.tsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import MuiAppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Drawer, { drawerWidth } from '@components/Drawer';
+import Link from '@components/Link';
+// import { pageLinks, IpageLinks } from '@utils/pageLinks';
+
+// const renderToolbarLinks = () => {
+//   const { about, githubRepo } = pageLinks;
+//   const appBarLinks: IpageLinks[] = [about, githubRepo];
+
+//   return (
+//     <>
+//       <Box sx={{ flexGrow: 1 }} />
+//       <Box sx={{ display: { xs: 'none', lg: 'flex' } }}>
+//         {/* eslint-disable-next-line object-curly-newline */}
+//         {appBarLinks.map(({ linkName, url, icon }) => (
+//           <Link key={linkName} href={url} rel='noreferrer' color='#fff'>
+//             <Tooltip key={linkName} title={linkName}>
+//               <IconButton key={linkName} size='large' color='inherit'>
+//                 {icon}
+//               </IconButton>
+//             </Tooltip>
+//           </Link>
+//         ))}
+//       </Box>
+//     </>
+//   );
+// };
+
+/**
+ * Appbar used for navigating to different pages.
+ * Also used for searching problems and users
+ */
+const AppBar = () => {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }} component='nav'>
+      <MuiAppBar
+        position='fixed'
+        sx={{
+          width: { lg: `calc(100% - ${drawerWidth}px)` },
+          ml: { lg: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          {/* app drawer */}
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='open drawer'
+            sx={{ mr: 2, display: { sm: 'block', lg: 'none' } }}
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* ---------------------------------------------------------------------------------- */}
+          <Link
+            href='/'
+            variant='h6'
+            noWrap
+            // sx={{ display: { xs: 'block' } }}
+            color='#fff'
+          >
+            Pi-Hole
+          </Link>
+          {/* ---------------------------------------------------------------------------------- */}
+          {/* {renderToolbarLinks} */}
+          {/* ---------------------------------------------------------------------------------- */}
+        </Toolbar>
+      </MuiAppBar>
+      {/* ---------------------------------------------------------------------------------- */}
+      {/* Drawer */}
+      <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+        <Drawer
+          variant='temporary'
+          open={isDrawerOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { sm: 'block', lg: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        />
+        <Drawer
+          variant='permanent'
+          ModalProps={{
+            keepMounted: false, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'none', lg: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default AppBar;
