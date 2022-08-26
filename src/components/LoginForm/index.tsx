60a886dd510e451660a2daac614f66d80d859c42
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-08-25 19:50:26 -0600

    feat(component): add component 'LoginForm'
      ## what
      - add login form
      - takes input
        - ip address
        - port number
        - password
    
      ## how
      - the component has states to store
        - ip address
        - port number
        - password
          - stored as sha256 hash
      - input validation
        - validateIp()
          - uses npm package 'ip-address' to verify ipv4 provided by the
            user is correct
          - if ip address is confirmed to be valid, component state
            'isIpValid' is set to true
          - if ip address is NOT valid, component state 'isIpValid' is set to false
        - validatePort()
          - checks:
            - if port number string is empty, set component state 'isPortValid' to false
            - if port number is a number, set component state 'isPortValid' to true
            - if port number is greater than 0, set component state 'isPortValid' to true
          - NOTE: pasting characters bypasses the checks. need to implement
            'onPaste' event
      - event handler
        - onChange()
          - this is called when following textfield contents has changed
            - ipAddress
              - sets component state property 'ipAddress'
              - calls function 'validateIp'
            - port number
              - sets component state property 'port'
              - calls function 'validatePort'
            - password
              - hash password using sha256
              - set component state property 'password'
                - if password from textfield is empty, set component
                  property 'password' to empty string
                  - why? this will be used to enable the submit button using
                    'password.length'
        - portOnKeyPress()
          - this is called when a key is pressed for 'port' textfield
            - if the key pressed is NOT number, then it will be blocked
      - handleSubmit()
        - once the button is pressed, a HTTP request is called to verify the
          user
        - TODO: if user is authenticated, redirect to 'Home' page
        - TODO: if user has failed to authenticate, display a message
      - submit button
        - enable submit button IF:
          - component state 'isIpValid' is true
          - component state 'isPortValid' is true
          - password string is NOT empty
        - run function 'handleSubmit' when pressing the submit button
    
      ## why
      - some of the api in Pi-hole requires user authentication
    
      ## where
      - file placed in './src/components/LoginForm/index.tsx'
    
      ## usage
    
      ## TODO
      - load a session cookie from server if the user is already
        authenticated
      - display error message if failed to authenticate

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,219 @@
+import { ChangeEvent, Component, FormEvent, KeyboardEvent } from 'react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import crypto from 'crypto';
+import { Address4 } from 'ip-address';
+
+import { PostRequestData } from '@pages/api/auth/login';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IProps {}
+
+/**
+ * Login Component state props
+ */
+interface IState extends PostRequestData {
+  /**
+   * Whether the Ipv4 address provided is valid.
+   * Using npm package 'ip-address' to verify
+   *
+   * @see {@link https://github.com/beaugunderson/ip-address}
+   */
+  isIpValid: boolean;
+  isPortValid: boolean;
+}
+
+/**
+ * Render Login component
+ */
+class LoginForm extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      ipAddress: '',
+      password: '',
+      port: '80',
+      isIpValid: true,
+      isPortValid: true,
+    };
+  }
+
+  /**
+   * Check if Component state property 'ipAddress' is a valid IPv4 address.
+   * If it's valid, then set Component state property 'isIpValid' to true,
+   * If it's not valid, then set Component state property 'isIpValid' to false,
+   *
+   * Uses npm package 'ip-address to check it's a property IPv4 address
+   * @see {@link https://github.com/beaugunderson/ip-address}
+   */
+  validateIp = () => {
+    const { ipAddress } = this.state;
+
+    if (Address4.isValid(ipAddress)) {
+      this.setState({ isIpValid: true });
+    } else {
+      this.setState({ isIpValid: false });
+    }
+  };
+
+  /**
+   * Check if component state property 'port' is a valid number
+   *
+   * If it's valid, then set Component state property 'isPortValid' to true
+   * If it's not valid, then set Component state property 'isPortValid' to false
+   *
+   */
+  validatePort = () => {
+    const { port } = this.state;
+
+    if (port === '') {
+      this.setState({ isPortValid: false });
+    } else if (Number.isInteger(+port) && +port > 0) {
+      this.setState({ isPortValid: true });
+    } else {
+      this.setState({ isPortValid: false });
+    }
+  };
+
+  /**
+   * Set Component state values when input text changes.
+   * Sets Component state property 'ipAddress'
+   * Sets Component state property 'password'
+   *
+   * @param event - Event when changing input value
+   */
+  onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    if (name === 'ipAddress') {
+      this.setState({ ipAddress: value }, this.validateIp);
+    } else if (name === 'password') {
+      // store password as a hash. NEVER in plain text
+      const hash = value.length > 0 ? crypto.createHash('sha256').update(value).digest('hex') : '';
+      this.setState({ password: hash });
+    } else if (name === 'port') {
+      this.setState({ port: value }, this.validatePort);
+    }
+  };
+
+  /**
+   * Check if the key pressed is a number. Block everything else.
+   * To be used ONLY on Textfield for 'port'
+   *
+   * @param event - Event when key is pressed
+   */
+  // eslint-disable-next-line class-methods-use-this
+  portOnKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    const { which } = event;
+
+    // check the ascii value
+    // Decimal 0 = 48 ascii
+    // Decimal 9 = 57 ascii
+    // obtained from
+    //    https://thewebdev.info/2022/06/15/how-to-prevent-typing-non-numeric-characters-in-input-type-number-with-javascript/
+    //
+    // check if character is a non-numeric
+    if (which < 48 || which > 57) {
+      // stop character from appending
+      event.preventDefault();
+    }
+  };
+
+  /**
+   * Call api '/api/auth/login' to authenticate
+   * If the authentication was successful, then redirect page to 'dashboard' (home page)
+   * If the authentication has failed, display an error message
+   *
+   * @param event - Event when submitting form
+   */
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const { ipAddress, password, port } = this.state;
+
+    event.preventDefault();
+
+    axios
+      .post('/api/auth/login', {
+        ipAddress,
+        password,
+        port,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    const { isPortValid, isIpValid, password, port } = this.state;
+
+    return (
+      <form method='POST' action='/api/auth/login' onSubmit={this.handleSubmit}>
+        <Grid container direction='column' spacing={2}>
+          <Grid item>
+            <Grid container direction='row' spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  autoFocus
+                  type='text'
+                  id='pihole-server-ip'
+                  name='ipAddress'
+                  placeholder='192.168.1.22'
+                  label='Pi-Hole IPv4 address'
+                  error={!isIpValid}
+                  {...(!isIpValid ? { helperText: 'Invalid IPv4 Address' } : {})}
+                  onBlur={this.validateIp}
+                  onChange={this.onChange}
+                  autoComplete='off'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  type='text'
+                  id='pihole-server-port'
+                  name='port'
+                  placeholder='80'
+                  label='Pi-Hole port number'
+                  onChange={this.onChange}
+                  autoComplete='off'
+                  fullWidth
+                  inputProps={{ inputMode: 'numeric', pattern: '\\d*' }}
+                  error={!isPortValid}
+                  {...(!isPortValid ? { helperText: 'Must be a number' } : {})}
+                  value={port}
+                  onBlur={this.validatePort}
+                  onKeyPress={this.portOnKeyPress}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              type='password'
+              id='pihole-password'
+              name='password'
+              label='Password'
+              onChange={this.onChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              fullWidth
+              variant='contained'
+              type='submit'
+              disabled={!isIpValid || password.length === 0 || !isPortValid}
+            >
+              Log in
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
