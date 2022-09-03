592bfc283eb4b6aeefa2b7fe979dff0b6fc519be
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-02 20:17:15 -0600

    feat(api): store authenticated credentials to an encrypted session cookie
      ## what
      - use 'iron-session' to create authenticated session cookie
      - create 'SessionOptions' object in './src/lib/AuthSession/index.ts'
      - create at least 32 character long password in .env.local 'SECRET_COOKIE_PASSWORD' used for encryption
    
      ## how
      - ./src/lib/AuthSession/index.ts
        - create SessionOptions object to setup Cookie configs
          - password: use .env.local 'SECRET_COOKIE_PASSWORD' to encrypt
            session cookie contents
            - use at least 32 character long password
          - cookie name
          - cookie options
            - cookie age: 30 minutes in the form of seconds
        - declare iron-session module interface 'IronSessionData' to link
          'IAuthSession' to iron-session
          - this is mainly used for typescript type definitions
      - ./src/pages/api/auth/login.ts
        - use higher order function to inject iron-session to the request
          being handled using function 'withIronSessionApiRoute'
          - takes in function to handle requests and 'SessionOptions' object
        - after confirming the user is authenticated, store user credentials
          in iron-session
          - ipAddress: address to Pi-hole
          - port: port number of Pi-hole web service
          - password: hashed password of the user of Pi-hole
    
      ## why
      - this a way of storing pi-hole credentials in an encrypted form
      - this will allow the ability to send HTTP requests that require an
        authenticated user
    
      ## where
    
      ## usage

--- src/lib/AuthSession/index.ts
+++ src/lib/AuthSession/index.ts
@@ -1,3 +1,6 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
+import type { IronSessionOptions } from 'iron-session';
+
 /**
  * Iron session data format to be used
  */
@@ -15,4 +18,22 @@ export interface IAuthSession {
    * Stored as sha256sum
    */
   password: string;
+}
+
+export const sessionOptions: IronSessionOptions = {
+  password: process.env.SECRET_COOKIE_PASSWORD as string,
+  cookieName: 'iron-session/pihole/auth',
+  // https://github.com/vvo/iron-session#ironoptions
+  cookieOptions: {
+    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 30 * 60 - 60, // 30 minutes in seconds
+  },
+};
+
+// This is where we specify the typings of req.session.*
+declare module 'iron-session' {
+  interface IronSessionData {
+    authSession: IAuthSession;
+  }
 }
