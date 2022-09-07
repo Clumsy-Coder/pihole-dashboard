3ac8974f9e89aa7d709783aeb857ffbc61726c0c
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 19:47:46 -0600

    feat(api): create helper functions for 'iron-session'
      ## what
      - create helper functions when using 'iron-session'
        - ironSessionTTL: time the cookie will be valid for in seconds
        - withSessionRoute: Wrapping function used for iron-session in NextJS api routes
        - withSessionSsr: Wrapping function used for getServerSideProps function on client side
    
      ## how
      - followed a guide from
        - https://github.com/vvo/iron-session#session-wrappers
    
      ## why
      - wrapping the api route to use iron-session can be offloaded to a wrapping function,
        - this will reduce the number of issues
      - wrapping the iron-session for client side makes it easier to use iron-session cookies
    
      ## where
    
      ## usage

--- src/lib/AuthSession/index.ts
+++ src/lib/AuthSession/index.ts
@@ -1,5 +1,7 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { IronSessionOptions } from 'iron-session';
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextApiHandler } from 'next';
 
 /**
  * Iron session data format to be used
@@ -20,14 +22,24 @@ export interface IAuthSession {
   password: string;
 }
 
+/**
+ * Time the cookie will be valid for in seconds.
+ */
+const ironSessionTTL = 30 * 60;
+
+/**
+ * Iron session configs
+ */
 export const sessionOptions: IronSessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD as string,
   cookieName: 'iron-session/pihole/auth',
+  ttl: ironSessionTTL,
   // https://github.com/vvo/iron-session#ironoptions
   cookieOptions: {
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 30 * 60 - 60, // 30 minutes in seconds
+    // https://github.com/vvo/iron-session#session-cookies
+    // maxAge: undefined // session expires when closing window/tab.
   },
 };
 
@@ -36,4 +48,64 @@ declare module 'iron-session' {
   interface IronSessionData {
     authSession: IAuthSession;
   }
+}
+
+/**
+ * Wrapping function used for iron-session in NextJS api routes
+ *
+ * @example
+ * ```typescript
+ * // /api/auth/login
+ *
+ * import { withSessionRoute } from '@lib/AuthSession
+ *
+ * const requestHandler = async (req: NextApiRequest, res: NextApiResponse) => { ... }
+ *
+ * withSessionRoute(requestHandler)
+ * ```
+ */
+export function withSessionRoute(handler: NextApiHandler) {
+  return withIronSessionApiRoute(handler, sessionOptions);
+}
+
+/**
+ * Wrapping function used for getServerSideProps function on client side
+ *
+ * @example
+ * ```typescript
+ * // pages/login/index.tsx
+ *
+ * import { GetServerSidePropsContext } from 'next';
+ * import { withSessionSsr } from '@lib/AuthSession'
+ *
+ * const component = () => (<div>component</div>)
+ *
+ * const serverPropsHandler = ({request}: GetServerSidePropsContext => {
+ *    const authSession = request.session.authSession
+ *
+ *    // if already logged in, go to home page
+ *    if(authSession) {
+ *      return {
+ *         redirect: {
+ *           destination: '/',
+ *           permanent: true,
+ *         },
+ *       };
+ *    }
+ * })
+ *
+ * const getServerSideProps = withSessionSsr(serverPropsHandler)
+ *
+ * ```
+ *
+ * @param handler - getServerSideProps handler
+ * @returns iron-session wrapped in getServerSideProps function
+ */
+// Thesis types are compatible with InferGetStaticPropsType https://nextjs.org/docs/basic-features/data-fetching#typescript-use-getstaticprops
+export function withSessionSsr<P extends Record<string, unknown> = Record<string, unknown>>(
+  handler: (
+    context: GetServerSidePropsContext,
+  ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
+) {
+  return withIronSessionSsr(handler, sessionOptions);
 }
