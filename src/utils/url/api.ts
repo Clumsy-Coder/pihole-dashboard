ed25ba34e0464589b275e01e42ec422ad6bb2a86
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 20:11:07 -0600

    chore(url): add API url constants
      ## what
      - add API url constants
        - getAuthSessionUrl
        - postAuthSessionUrl
        - deleteAuthSessionUrl
    
      ## how
    
      ## why
    
      ## where
    
      ## usage

--- src/utils/url/api.ts
+++ src/utils/url/api.ts
@@ -2,5 +2,16 @@
  * URL for fetching session
  * @returns URL for fetching session
  */
-export const fetchSessionUrl = () => `/api/auth/session`;
+export const getAuthSessionUrl = () => `/api/auth/session`;
 
+/**
+ * URL for logging in to Pi-hole
+ * @returns URL for logging in
+ */
+export const postAuthSessionUrl = () => `/api/auth/login`;
+
+/**
+ * URL for logging out of Pi-hole
+ * @returns URL for logging out
+ */
+export const deleteAuthSessionUrl = () => `/api/auth/logout`;
