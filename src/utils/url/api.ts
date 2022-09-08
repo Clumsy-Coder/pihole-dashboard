0ab95f0219fa9d195b1e0623ddf2318cdf84f291
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 18:01:13 -0600

    refactor(url): convert API url from function to string
      ## what
      - convert API url from arrow function to string
    
      ## how
    
      ## why
      - these urls will be used by Redux Toolkit Query
        - the RTK query doesn't need to take the base of the url '/api
    
      ## where
    
      ## usage

--- src/utils/url/api.ts
+++ src/utils/url/api.ts
@@ -1,17 +1,14 @@
 /**
  * URL for fetching session
- * @returns URL for fetching session
  */
-export const getAuthSessionUrl = () => `/api/auth/session`;
+export const getAuthSessionUrl = `auth/session`;
 
 /**
  * URL for logging in to Pi-hole
- * @returns URL for logging in
  */
-export const postAuthSessionUrl = () => `/api/auth/login`;
+export const postAuthSessionUrl = `auth/login`;
 
 /**
  * URL for logging out of Pi-hole
- * @returns URL for logging out
  */
-export const deleteAuthSessionUrl = () => `/api/auth/logout`;
+export const deleteAuthSessionUrl = `auth/logout`;
