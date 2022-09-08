efdffc7b9e7a5a83e87f181fac5e8829b7d51fcb
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 19:36:11 -0600

    feat(api): add API endpoint '/api/auth/session'
      ## what
      - add API endpoint for /api/auth/session
      - used for getting the current session
    
      ## how
      - get the current session
        - if a session cookie exists, then return the IP address and port number
        - if a session cookie doesn't exist, then return empty IP address and port number
          - ex:
            ```json
            {
              "ipAddress": "",
              "port": "",
              "password": ""
            }
            ```
    
      ## why
      - this will be used as a way to check if the user is authenticated.
      - later on this will be used to determine whether to redirect to
        Login page or continue on the current page route
      - later on this will be used to protect certain page routes,
        - if the user is NOT authenticated, then redirect to the Login page
    
      ## where
    
      ## usage

---
+++ src/pages/api/auth/session.ts
@@ -0,0 +1,75 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { IAuthSession, sessionOptions } from '@lib/AuthSession';
+import logger from '@utils/logger';
+
+/**
+ * Get Login session.
+ * If there's a session cookie, it will return ip address and port number.
+ * If there's no session cookie, it will return empty strings for ip address and port number.
+ *
+ * @param req - HTTP request provided by NextJS
+ * @param res - HTTP response provided by NextJS
+ */
+const handleGet = (req: NextApiRequest, res: NextApiResponse<IAuthSession>) => {
+  const getLogger = logger.scope('/api/auth/session', 'GET');
+
+  getLogger.info(`checking is auth session exists`);
+
+  if (req.session.authSession) {
+    const { ipAddress, port } = req.session.authSession;
+    const resObj: IAuthSession = {
+      ipAddress,
+      port,
+      password: '',
+    };
+
+    getLogger.complete(`auth session found. returning '${JSON.stringify(resObj, null, 2)}'`);
+
+    // in a real world application you might read the user id from the session and then do a database request
+    // to get more information on the user if needed
+    res.status(200).json(resObj);
+  } else {
+    const resObj: IAuthSession = {
+      ipAddress: '',
+      port: '',
+      password: '',
+    };
+
+    getLogger.complete(`auth session NOT found. returning ${JSON.stringify(resObj, null, 2)}`);
+
+    res.status(200).json(resObj);
+  }
+};
+
+/**
+ * Default method to run when executing this http api endpoint
+ *
+ * @remarks
+ * HTTP API endpoint `/api/auth/login`
+ *
+ * @remarks
+ * HTTP method allowed: `POST`
+ */
+const mainHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method = '' } = req;
+
+  // limit which HTTP methods are allowed
+  switch (method) {
+    case 'GET': {
+      handleGet(req, res);
+      break;
+    }
+    default: {
+      logger.error({
+        prefix: `/api/auth/session`,
+        message: `invalid HTTP method type '${method}'`,
+      });
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  }
+};
+
+export default withIronSessionApiRoute(mainHandler, sessionOptions);
