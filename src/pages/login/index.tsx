7c4bf8e9e036cee4f786ba025d86acd57f46fdf3
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 19:29:11 -0600

    style(page): remove unused code
      ## what
    
      ## how
    
      ## why
    
      ## where
    
      ## usage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,27 +1,18 @@
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import SvgIcon from '@mui/material/SvgIcon';
-import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import { withIronSessionSsr } from 'iron-session/next';
 
 import PiholeLogo from '@svg/pihole-logo.svg';
 import LoginForm from '@components/LoginForm';
-
-const PaperStyled = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(5),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  // width: '90%',
-}));
+import { sessionOptions } from '@lib/AuthSession/index';
+import { GetServerSidePropsContext } from 'next';
 
 /**
  * Functional component for login page
  * @returns Functional Component for Login page
  */
 const PageLogin = () => (
-  // <PaperStyled>
   <Grid container direction='column' justifyContent='center' alignItems='center' spacing={2}>
     <Grid item>
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
@@ -34,7 +25,6 @@ const PageLogin = () => (
       <LoginForm />
     </Grid>
   </Grid>
-  // </PaperStyled>
 );
 
 export default PageLogin;
