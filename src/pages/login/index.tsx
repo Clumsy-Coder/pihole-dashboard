a0a37e6fc6015014047a814181f0b7a36f8c3437
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-10 17:00:15 -0600

    feat(page): redirect login page to Home page if the user is logged in
      ## what
      - redirect login page to home page IF the user is logged in
    
      ## how
      - using the function `withSessionSsr` in `src/lib/AuthSession/index.ts`
        - check if a session cookie from iron-session is set
        - if it's set, redirect to the home page
    
      ## why
      - no need to display the login page IF the user is already logged in.
    
      ## where
    
      ## usage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,17 @@
 import Grid from '@mui/material/Grid';
 import SvgIcon from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
-import { withIronSessionSsr } from 'iron-session/next';
+import { GetServerSidePropsContext, NextPage } from 'next';
 
 import PiholeLogo from '@svg/pihole-logo.svg';
 import LoginForm from '@components/LoginForm';
-import { sessionOptions } from '@lib/AuthSession/index';
-import { GetServerSidePropsContext } from 'next';
+import { withSessionSsr } from '@lib/AuthSession/index';
 
 /**
  * Functional component for login page
  * @returns Functional Component for Login page
  */
-const PageLogin = () => (
+const PageLogin: NextPage = () => (
   <Grid container direction='column' justifyContent='center' alignItems='center' spacing={2}>
     <Grid item>
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
@@ -26,5 +25,29 @@ const PageLogin = () => (
     </Grid>
   </Grid>
 );
+
+/**
+ * Server side rendering function.
+ *
+ * If the user is logged in, it will redirect to the home page
+ */
+export const getServerSideProps = withSessionSsr((context: GetServerSidePropsContext) => {
+  const { authSession } = context.req.session;
+
+  // redirect to home page if the user is logged in
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (authSession) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+});
 
 export default PageLogin;
