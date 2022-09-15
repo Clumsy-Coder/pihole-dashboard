7c3e9c8f88219e8e3ccfe1bbd712e351dc1c0be7
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-15 15:36:55 -0600

    refactor(redux): create enum 'HttpQueryTypes' for RTK Query
      ## what
      - Create enum for HTTP query types
      - Use enum on
        - AuthSession RTK Query
        - Summary RTK Query
    
      ## how
      - Create enum with its values of HTTP method types
      - use the enum
    
      ## why
      - There's a chance for bugs to occur if using strings when defining
        the method type to use in RTK Query
      - Easy to use
    
      ## where
      - src/redux/AuthSession/index.ts
      - src/redux/Summary/index.ts
      - src/redux/apiSlice.ts
    
      ## usage

--- src/redux/Summary/index.ts
+++ src/redux/Summary/index.ts
@@ -1,4 +1,4 @@
-import apiSlice, { TagTypes } from '@redux/apiSlice';
+import apiSlice, { TagTypes, HttpQueryType } from '@redux/apiSlice';
 import { getSummaryUrl, getSummaryRawUrl } from '@utils/url/api';
 import { ISummary, ISummaryRaw } from '@utils/url/upstream.types';
 
@@ -8,7 +8,7 @@ const summaryApi = apiSlice.injectEndpoints({
     getSummary: build.query<ISummary, void>({
       query: () => ({
         url: getSummaryUrl,
-        method: 'GET',
+        method: HttpQueryType.GET,
       }),
       providesTags: () => [{ type: TagTypes.SUMMARY }],
     }),
@@ -16,7 +16,7 @@ const summaryApi = apiSlice.injectEndpoints({
     getSummaryRaw: build.query<ISummaryRaw, void>({
       query: () => ({
         url: getSummaryRawUrl,
-        method: 'GET',
+        method: HttpQueryType.GET,
       }),
       providesTags: () => [{ type: TagTypes.SUMMARY_RAW }],
     }),
