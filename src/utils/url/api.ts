93783f97595f12f6538bea929a90bce225836e91
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-15 12:00:00 -0600

    chore(url): add URL constant for '/api/auth/unauthorized'
      ## what
      - add URL constant for '/api/auth/unauthorized'
    
      ## how
    
      ## why
      - This will be used by protected API routes
    
      ## where
    
      ## usage

--- src/utils/url/api.ts
+++ src/utils/url/api.ts
@@ -12,3 +12,9 @@ export const postAuthSessionUrl = `auth/login`;
  * URL for logging out of Pi-hole
  */
 export const deleteAuthSessionUrl = `auth/logout`;
+
+/**
+ * URL for getting message user is not Authenticated.
+ * To be used by protected API endpoints
+ */
+export const getUnauthorizedUrl = 'auth/unauthorized';
