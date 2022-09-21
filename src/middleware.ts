6eb970d1f007f92c766eb1f205c8a837b54d7eec
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-21 16:23:55 -0600

    feat(middleware): add NextJS middleware
      ## what
      - add NextJS middleware
      - redirect `api` requests to `/api/auth/unauthorized` if AuthSession is found for protected routes
      - redirect `page` requests to `/login` if AuthSession is found for protected routes
    
      ## how
      - protected routes
        - define an array of protected routes
      - check if it's api request
        - redirect to `/api/auth/unauthorized` if AuthSession is not set
      - check if it's `page` request
        - redirect to `/login` if AuthSession is not set
    
      ## why
      - easier way to protect api and page routes
    
      ## where
      - ./src/middleware.ts
    
      ## usage
    
      ## NOTE
      - moving the NextJS middleware `config` export to another file
        - the protected routes array can be used somewhere else, but NextJS
          prevents importing from another file.
          - not sure why
          - check  https://github.com/vvo/iron-session/issues/543

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getIronSession } from 'iron-session/edge';
+
+import { sessionOptions } from '@lib/AuthSession/Config';
+
+/**
+ * NextJS middleware used for redirecting
+ * - Non authenticated API calls to `/api/auth/unauthorized`
+ * - Non authenticated page load to `/login` page
+ */
+export const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next();
+  const { authSession } = await getIronSession(req, res, sessionOptions);
+  const url = req.nextUrl.pathname;
+
+  // if accessing API url
+  if (url.startsWith('/api')) {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (authSession === undefined) {
+      return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+    }
+  }
+  // if accessing pages URL
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  else if (authSession === undefined) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return res;
+};
+
+export const config = {
+  matcher: [
+    // api routes
+    '/api/summary',
+    '/api/forwardedDestinations',
+
+    // page routes
+    '/',
+  ],
+};
