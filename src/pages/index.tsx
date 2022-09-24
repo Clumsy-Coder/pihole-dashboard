f9c2424e424416e66180ad0184fffb8238749626
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-24 08:46:34 -0600

    feat(redux): fetch `forwardedDestinations` in an interval
      ## what
      - fetch data from api `/api/forwardedDestinations` in interval
    
      ## how
      - add config when using React hook to fetch data in an interval
        - `pollingInterval`: fetch data in 2 seconds (written in milliseconds)
    
      ## why
    
      ## where
      - ./src/pages/index.tsx
    
      ## usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,16 @@ import Head from 'next/head';
 
 import Summary from '@components/Summary';
 import { useGetSummaryQuery } from '@redux/Summary';
+import { useGetForwardedDestinationsQuery } from '@redux/ForwardedDestinations';
 
 const Home: NextPage = () => {
   const { data: summaryData, isLoading: summaryIsLoading } = useGetSummaryQuery(undefined, {
     pollingInterval: 2 * 1000,
   });
+  const { data: forwardedDestinationData, isLoading: forwardedDestinationsIsLoading } =
+    useGetForwardedDestinationsQuery(undefined, {
+      pollingInterval: 2 * 1000,
+    });
 
   return (
     <Box sx={{ flexGrow: 1 }}>
