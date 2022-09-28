3de8f120f90bfcb22338d699b74de1b620b7d24a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-28 16:40:14 -0600

    chore(env): add env 'NEXT_PUBLIC_POLLING_QUERY_TYPES' to generateEnvs.sh
      ## what
      - add env `NEXT_PUBLIC_POLLING_QUERY_TYPES` to `generateEnvs.sh`
    
      ## how
      - set the polling time to `60000` ms
    
      ## why
      - this polling time was set in Pi-hole dashboard
        - check https://github.com/pi-hole/AdminLTE/blob/b110eccceac2473697f8fac59e5de5d12c08c56e/scripts/pi-hole/js/index.js#L338
    
      ## where
      - ./generateEnvs.sh
    
      ## usage

--- generateEnvs.sh
+++ generateEnvs.sh
@@ -11,3 +11,4 @@ echo "SECURE_COOKIE_TTL=$(expr 60 \* 60)" >> .env.local;
 echo "NEXT_PUBLIC_POLLING_AUTH_SESSION=1000" >> .env.local
 echo "NEXT_PUBLIC_POLLING_SUMMARY=2000" >> .env.local
 echo "NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS=60000" >> .env.local
+echo "NEXT_PUBLIC_POLLING_QUERY_TYPES=60000" >> .env.local
