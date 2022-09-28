20e916da2f5bfe2a455310733d09850a2aa9066a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-28 09:33:11 -0600

    chore(env): generate fetch polling ENVs when running `generateEnvs.sh`
      ## what
      - add ENV when running `generateEnvs.sh` script
        - add ENV 'NEXT_PUBLIC_POLLING_AUTH_SESSION=1000' to `.env.local`
        - add ENV 'NEXT_PUBLIC_POLLING_SUMMARY=2000' to `.env.local`
        - add ENV 'NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS=2000' to `.env.local`
    
      ## how
    
      ## why
      - it's better to control polling times when fetching data from api
      - makes it easy to change polling time without changing code
    
      ## where
      - ./generateEnvs.sh
    
      ## usage

--- generateEnvs.sh
+++ generateEnvs.sh
@@ -7,3 +7,7 @@ echo -n '' > .env.local # empty the .env.local file
 echo "NEXT_PUBLIC_BUILD_VERSION=$(npm run -s generateVersion)" >> .env.local;
 echo "NEXT_PUBLIC_BUILD_ID=$(npm run -s generateBuildId)" >> .env.local;
 echo "SECRET_COOKIE_PASSWORD=cteBV25ymu4HnVAvhDVf46JbGxZePA6Q" >> .env.local;
+echo "SECURE_COOKIE_TTL=$(expr 60 \* 60)" >> .env.local;
+echo "NEXT_PUBLIC_POLLING_AUTH_SESSION=1000" >> .env.local
+echo "NEXT_PUBLIC_POLLING_SUMMARY=2000" >> .env.local
+echo "NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS=2000" >> .env.local
