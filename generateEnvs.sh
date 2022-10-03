4f86376ca4681d35cac57bfa15645b7a32bc1536
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-03 12:09:40 -0600

    docs(env): add comments for adding ENVs
      ## what
      - add comments for adding ENVs
        - polling time in milliseconds
        - iron-session ttl in seconds
        - section header. description of what a collection ENVs are used for
    
      ## how
    
      ## why
      - add more documentation on what a collection of ENVs are used for
      - add comment on what unit the time ENVs are used for
    
      ## where
      - ./generateEnvs.sh
    
      ## usage

--- generateEnvs.sh
+++ generateEnvs.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+# creates ENVs and puts them into .env.local file
+# NOTE: this is ONLY used for development purposes only.
+
 set -x
 # generate ENVs
 
 echo -n '' > .env.local # empty the .env.local file
 echo "NEXT_PUBLIC_BUILD_VERSION=$(npm run -s generateVersion)" >> .env.local;
 echo "NEXT_PUBLIC_BUILD_ID=$(npm run -s generateBuildId)" >> .env.local;
+
+# iron session
 echo "SECRET_COOKIE_PASSWORD=cteBV25ymu4HnVAvhDVf46JbGxZePA6Q" >> .env.local;
-echo "SECURE_COOKIE_TTL=$(expr 60 \* 60)" >> .env.local;
+echo "SECURE_COOKIE_TTL=$(expr 60 \* 60)" >> .env.local;                      # time in seconds
 echo "NEXT_PUBLIC_POLLING_AUTH_SESSION=1000" >> .env.local
-echo "NEXT_PUBLIC_POLLING_SUMMARY=2000" >> .env.local
-echo "NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS=60000" >> .env.local
-echo "NEXT_PUBLIC_POLLING_QUERY_TYPES=60000" >> .env.local
-echo "NEXT_PUBLIC_POLLING_TOP_PERMITTED_QUERIES=10000" >> .env.local
-echo "NEXT_PUBLIC_POLLING_TOP_BLOCKED_QUERIES=10000" >> .env.local
+
+# fetching from /api/summary
+echo "NEXT_PUBLIC_POLLING_SUMMARY=2000" >> .env.local                         # time in milliseconds
+
+# fetching from /api/forwardedDestinations
+echo "NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS=60000" >> .env.local         # time in milliseconds
+
+# fetching from /api/queryTypes
+echo "NEXT_PUBLIC_POLLING_QUERY_TYPES=60000" >> .env.local                    # time in milliseconds
+
+# top permitted and blocked queries
+echo "NEXT_PUBLIC_POLLING_TOP_PERMITTED_QUERIES=10000" >> .env.local          # time in milliseconds
+echo "NEXT_PUBLIC_POLLING_TOP_BLOCKED_QUERIES=10000" >> .env.local            # time in milliseconds
 echo "NEXT_PUBLIC_NUM_ENTRIES_TOP_PERMITTED_QUERIES=10" >> .env.local
 echo "NEXT_PUBLIC_NUM_ENTRIES_TOP_BLOCKED_QUERIES=10" >> .env.local
