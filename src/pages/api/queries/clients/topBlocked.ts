bc69312c17b536062153f32828b3e85378d0397b
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-03 18:22:05 -0600

    feat(api): add NextJS 'GET' API endpoint for `/api/queries/clients/topBlocked`
      ## what
      - add 'GET' API endpoint for `/api/queries/clients/topBlocked`
    
      ## how
      - Fetch data from upstream Pi-hole API
    
      ## why
      - This API endpoint will be used to fetch 'top clients blocked queries' data using Redux Toolkit Query
    
      ## where
      - ./src/pages/api/queries/clients/topBlocked.ts
    
      ## usage

---
+++ src/pages/api/queries/clients/topBlocked.ts
@@ -0,0 +1,99 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { withSessionRoute } from '@lib/AuthSession';
+import logger from '@utils/logger';
+import { topBlockedClientsUrl } from '@utils/url/upstream';
+import { ITopBlockedClientsData, ITopBlockedClients } from '@utils/url/upstream.types';
+import { getTopBlockedClientsUrl as apiUrl } from '@utils/url/api';
+
+/**
+ * Error message to return to the Requester
+ */
+interface ErrorMessage {
+  message: string;
+}
+
+/**
+ * API endpoint query parameters
+ */
+export interface IGetRequestData {
+  /**
+   * The number of entries to return
+   *
+   * @remarks defaults to 10
+   */
+  numEntries?: number;
+}
+
+/**
+ * Summary 'formatted' data to return
+ */
+export type IGetTopClientsResponseData = ITopBlockedClientsData;
+
+/**
+ * GET endpoint for `/api/queries/clients/topBlocked`
+ *
+ * @remarks
+ * Returns forwarded destinations query data
+ *
+ * Must be authenticated
+ * @param req - HTTP request provided by NextJS
+ * @param res - HTTP response provided by NextJS
+ */
+const handleGetTopBlockedClients = (
+  req: NextApiRequest,
+  res: NextApiResponse<IGetTopClientsResponseData | ErrorMessage>,
+) => {
+  const getLogger = logger.scope(apiUrl, 'GET');
+  const { ipAddress, port, password } = req.session.authSession;
+  const { numEntries = 10 } = req.query as IGetRequestData;
+
+  const requestUrl = `http://${ipAddress}:${port}/${topBlockedClientsUrl(
+    numEntries,
+  )}&auth=${password}`;
+
+  axios
+    .get<ITopBlockedClients>(requestUrl)
+    .then((response) => {
+      getLogger.info('data obtained from upstream');
+      getLogger.debug(`data: `, response.data.top_sources_blocked);
+      res.status(200).json(response.data.top_sources_blocked);
+      getLogger.complete(`sending response`);
+    })
+    .catch((error) => {
+      getLogger.error(`error returned when sending HTTP request to '${requestUrl}'`);
+      res.status(500).json({ message: JSON.stringify(error) });
+    });
+};
+
+/**
+ * Default method to run when executing this http api endpoint
+ *
+ * @remarks
+ * HTTP API endpoint `/api/queries/clients/topBlocked`
+ *
+ * @remarks
+ * HTTP method allowed: `GET`
+ */
+const requestHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method = '' } = req;
+
+  // limit which HTTP methods are allowed
+  switch (method) {
+    case 'GET': {
+      handleGetTopBlockedClients(req, res);
+      break;
+    }
+    default: {
+      logger.error({
+        prefix: apiUrl,
+        message: `invalid HTTP method type '${method}'`,
+      });
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  }
+};
+
+export default withSessionRoute(requestHandler);
