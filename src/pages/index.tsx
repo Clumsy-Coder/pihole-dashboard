3606cf1f8bf9490dc6585955bab53d884d9bc3c9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-16 17:02:21 -0600

    refactor(redux): provide default value when using ENV for data fetching on `home page`
      ## what
      - provide default values when using ENVs
        - NEXT_PUBLIC_POLLING_SUMMARY : 2000
        - NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS: 60000
        - NEXT_PUBLIC_POLLING_QUERY_TYPES: 60000
        - NEXT_PUBLIC_NUM_ENTRIES_TOP_PERMITTED_QUERIES: 10
        - NEXT_PUBLIC_POLLING_TOP_PERMITTED_QUERIES: 10000
        - NEXT_PUBLIC_NUM_ENTRIES_TOP_BLOCKED_QUERIES: 10
        - NEXT_PUBLIC_POLLING_TOP_BLOCKED_QUERIES: 10000
        - NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_ALLOWED_QUERIES: 10
        - NEXT_PUBLIC_POLLING_TOP_CLIENTS_ALLOWED_QUERIES: 10000
        - NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_BLOCKED_QUERIES: 10
        - NEXT_PUBLIC_POLLING_TOP_CLIENTS_BLOCKED_QUERIES: 10000
    
      ## how
    
      ## why
      - this is to be used as a backup in case the ENVs are not provided
    
      ## where
      - ./src/pages/index.tsx
    
      ## usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,29 +21,29 @@ import { useGetTopAllowedClientsQuery, useGetTopBlockedClientsQuery } from '@red
 
 const Home: NextPage = () => {
   const summaryQuery = useGetSummaryQuery(undefined, {
-    pollingInterval: process.env.NEXT_PUBLIC_POLLING_SUMMARY,
+    pollingInterval: process.env.NEXT_PUBLIC_POLLING_SUMMARY || 2000,
   });
   const forwardedDestinations = useGetForwardedDestinationsQuery(undefined, {
-    pollingInterval: process.env.NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS,
+    pollingInterval: process.env.NEXT_PUBLIC_POLLING_FORWARDED_DESTINATIONS || 60000,
   });
   const queryTypes = useGetQueryTypesQuery(undefined, {
-    pollingInterval: process.env.NEXT_PUBLIC_POLLING_QUERY_TYPES,
+    pollingInterval: process.env.NEXT_PUBLIC_POLLING_QUERY_TYPES || 60000,
   });
   const topPermittedQueries = useGetTopPermittedQueriesQuery(
-    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_PERMITTED_QUERIES,
-    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_PERMITTED_QUERIES },
+    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_PERMITTED_QUERIES || 10,
+    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_PERMITTED_QUERIES || 10000 },
   );
   const topBlockedQueries = useGetTopBlockedQueriesQuery(
-    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_BLOCKED_QUERIES,
-    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_BLOCKED_QUERIES },
+    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_BLOCKED_QUERIES || 10,
+    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_BLOCKED_QUERIES || 10000 },
   );
   const topAllowedClientQueries = useGetTopAllowedClientsQuery(
-    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_ALLOWED_QUERIES,
-    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_CLIENTS_ALLOWED_QUERIES },
+    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_ALLOWED_QUERIES || 10,
+    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_CLIENTS_ALLOWED_QUERIES || 10000 },
   );
   const topBlockedClientQueries = useGetTopBlockedClientsQuery(
-    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_BLOCKED_QUERIES,
-    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_CLIENTS_BLOCKED_QUERIES },
+    process.env.NEXT_PUBLIC_NUM_ENTRIES_TOP_CLIENTS_BLOCKED_QUERIES || 10,
+    { pollingInterval: process.env.NEXT_PUBLIC_POLLING_TOP_CLIENTS_BLOCKED_QUERIES || 10000 },
   );
 
   return (
