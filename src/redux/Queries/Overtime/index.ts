7736e8403fa1d5d38669d5ac6ab950e2ec9d0d7e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-25 13:28:10 -0600

    feat(redux): add API query for `/api/queries/overtime?formatted=true` using Redux Toolkit Query
      ## what
      - add API query for `/api/queries/overtime?formatted=true` using Redux Toolkit Query
      - Query
        - GET `queries/overtime?formatted=true`
          - fetch data from NextJS API url `/api/queries/overtime?formatted=true`
      - export prebuilt React hook `useGetQueriesOvertimeFormattedQuery`
    
      ## how
      - https://redux-toolkit.js.org/rtk-query/overview
      - https://redux-toolkit.js.org/rtk-query/usage/queries
      - https://redux-toolkit.js.org/rtk-query/usage/code-splitting
    
      ## why
      - Easy way to manage fetching data from API, etc
      - Less overhead to manage the code to handle these operations
      - most of the work is done for you
    
      ## where
      - ./src/redux/Queries/Overtime/index.ts
    
      ## usage

--- src/redux/Queries/Overtime/index.ts
+++ src/redux/Queries/Overtime/index.ts
@@ -1,5 +1,6 @@
 import apiSlice, { TagTypes, HttpQueryType } from '@redux/apiSlice';
 import { getQueriesOvertimeUrl } from '@utils/url/api';
+import { IGetQueriesOvertimeFormatted } from '@pages/api/queries/overtime';
 import { IOverTimeData10minutes } from '@utils/url/upstream.types';
 
 const clientsApi = apiSlice.injectEndpoints({
@@ -12,8 +13,16 @@ const clientsApi = apiSlice.injectEndpoints({
       }),
       providesTags: () => [{ type: TagTypes.QUERY_OVERTIME_RAW }],
     }),
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+    getQueriesOvertimeFormatted: build.query<IGetQueriesOvertimeFormatted, void>({
+      query: () => ({
+        url: `${getQueriesOvertimeUrl}?formatted=true`,
+        method: HttpQueryType.GET,
+      }),
+      providesTags: () => [{ type: TagTypes.QUERY_OVERTIME_FORMATTED }],
+    }),
   }),
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export const { useGetQueriesOvertimeRawQuery } = clientsApi;
+export const { useGetQueriesOvertimeRawQuery, useGetQueriesOvertimeFormattedQuery } = clientsApi;
