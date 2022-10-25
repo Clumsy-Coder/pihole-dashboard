08739305536e877835ab22c4e3882922189813ef
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-25 13:12:37 -0600

    feat(api): add NextJS 'GET' API endpoint for `/api/queries/overtime?formatted=true`
      ## what
      - add `GET` API endpoint for `/api/queries/overtime?formatted=true`
    
      ## how
      - Fetch data from upstream Pi-hole API
      - format data to be compatible with `ChartJS` and `react-chartjs-2`
      - Code inspired by Pi-hole Admin portal
        - https://github.com/pi-hole/AdminLTE/blob/master/scripts/pi-hole/js/index.js#L219-L286
    
      ## why
      - This API endpoint will be used to fetch `queries overtime formatted` data using Redux Toolkit Query
      - This API endpoint is used by `ChartJS` and `react-chartjs-2` to display data
    
      ## where
      - ./src/pages/api/queries/overtime.ts
    
      ## usage

--- src/pages/api/queries/overtime.ts
+++ src/pages/api/queries/overtime.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ChartData } from 'chart.js';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { withSessionRoute } from '@lib/AuthSession';
@@ -14,10 +15,24 @@ interface ErrorMessage {
   message: string;
 }
 
+export interface IGetRequestData {
+  /**
+   * Return formatted data that is compatible with ChartJS
+   *
+   * @see {@link https://www.chartjs.org/docs/latest/general/data-structures.html}
+   * @see {@link https://www.chartjs.org/docs/latest/charts/bar.html#example-dataset-configuration}
+   *
+   * @defaultValue `false`
+   */
+  formatted?: string | 'false';
+}
+
 /**
  * `overTimeData10mins` data format obtained from Pi-hole
  */
 export type IGetQueriesOvertimeResponseData = IOverTimeData10minutes;
+
+export type IGetQueriesOvertimeFormatted = ChartData<'bar', number[], number>;
 
 /**
  * GET endpoint for `/api/queries/overtime`
@@ -48,6 +63,73 @@ const handleGetQueriesOvertimeRaw = (
 };
 
 /**
+ * GET endpoint for `/api/queries/overtime`
+ *
+ * returns formatted data for ChartJS and react-chartjs-2
+ *
+ * @see {@link https://github.com/pi-hole/AdminLTE/blob/master/scripts/pi-hole/js/index.js#L219-L286 | Code inspired by Pi-hole Admin portal}
+ *
+ * @param req - HTTP request provided by NextJS
+ * @param res - HTTP response provided by NextJS
+ */
+const handleGetQueriesOvertimeFormatted = (
+  req: NextApiRequest,
+  res: NextApiResponse<IGetQueriesOvertimeFormatted | ErrorMessage>,
+) => {
+  const getLogger = logger.scope(apiUrl, 'GET');
+  const { ipAddress, port, password } = req.session.authSession;
+
+  const requestUrl = `http://${ipAddress}:${port}/${overTimeData10minsUrl()}&auth=${password}`;
+  const responseData: IGetQueriesOvertimeFormatted = {
+    labels: [], // unix time
+    datasets: [],
+  };
+
+  axios
+    .get<IOverTimeData10minutes>(requestUrl)
+    .then((response) => {
+      getLogger.info('data obtained from upstream');
+
+      const domainsOvertime = Object.entries(response.data.domains_over_time);
+      const adsOvertime = Object.entries(response.data.ads_over_time);
+
+      // add blocked  datasets group
+      responseData.datasets.push({
+        data: [],
+        backgroundColor: '#f56954',
+        label: 'Blocked DNS Queries',
+      });
+      // add permitted  datasets group
+      responseData.datasets.push({
+        data: [],
+        backgroundColor: '#00a65a',
+        label: 'Permitted DNS Queries',
+      });
+
+      for (let i = 0; i < domainsOvertime.length; i += 1) {
+        const time = +domainsOvertime[i][0]; // convert domainOvertime to type `number`
+        const blocked = adsOvertime[i][1];
+        const allowed = domainsOvertime[i][1] - adsOvertime[i][1];
+        const hour = // set date/time in UNIX format
+          time < 1200
+            ? new Date().setHours(Math.floor(time / 6), 10 * (time % 6), 0, 0)
+            : new Date(1000 * time).getTime();
+
+        responseData.labels?.push(hour);
+        responseData.datasets[0].data.push(blocked);
+        responseData.datasets[1].data.push(allowed);
+      }
+
+      res.status(200).json(responseData);
+      getLogger.complete(`sending response`);
+    })
+    .catch((error) => {
+      getLogger.error(`error returned when sending HTTP request to '${requestUrl}'`);
+      res.status(500).json({ message: JSON.stringify(error) });
+    });
+};
+
+/**
  * Default method to run when executing this http api endpoint
  *
  * @remarks
@@ -62,7 +144,13 @@ const requestHandler = (req: NextApiRequest, res: NextApiResponse) => {
   // limit which HTTP methods are allowed
   switch (method) {
     case 'GET': {
-      handleGetQueriesOvertimeRaw(req, res);
+      const { formatted = 'false' } = req.query as IGetRequestData;
+
+      if (formatted === 'true') {
+        handleGetQueriesOvertimeFormatted(req, res);
+      } else {
+        handleGetQueriesOvertimeRaw(req, res);
+      }
       break;
     }
     default: {
