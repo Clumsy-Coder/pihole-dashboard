487fd5eb9972231dd558544043692602fd4cb617
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-30 09:24:59 -0600

    feat(api): add NextJS `GET` API endpoint for `/api/queries/clients/overtime?formatted=true`
      ## what
      - add `GET` API endpoint for `/api/queries/clients/overtime?formatted=true`
        - this will return formatted data obtained from upstream Pi-hole API
    
      ## how
      - fetch data from upstream Pi-hole API
      - format data to be compatible with `ChartJS` and `react-chartjs-2`
      - Code inspired by Pi-Hole Admin portal
        - https://github.com/pi-hole/AdminLTE/blob/master/scripts/pi-hole/js/index.js#L330-L417
    
      ## why
      - this API endpoint will be used to fetch `clients overtime` and `client names` data using Redux Toolkit Query
      - this API endpoint is used by `ChartJS` and `react-chartjs-2` to display data
    
      ## where
      - ./src/pages/api/queries/clients/overtime.ts
    
      ## usage

--- src/pages/api/queries/clients/overtime.ts
+++ src/pages/api/queries/clients/overtime.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { ChartData } from 'chart.js';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { withSessionRoute } from '@lib/AuthSession';
+import { chartThemeColours } from '@utils/darkTheme';
 import logger from '@utils/logger';
 import { clientOvertimeAndNamesUrl } from '@utils/url/upstream';
 import { IClientNames, IClientsOvertime } from '@utils/url/upstream.types';
@@ -14,10 +16,24 @@ interface ErrorMessage {
   message: string;
 }
 
+export interface IGetRequestData {
+  /**
+   * Return formatted data that is compatible with ChartJS
+   *
+   * @see {@link https://www.chartjs.org/docs/latest/general/data-structures.html}
+   * @see {@link https://www.chartjs.org/docs/latest/charts/bar.html#example-dataset-configuration}
+   *
+   * @defaultValue `false`
+   */
+  formatted?: string | 'false';
+}
+
 /**
  * `overtimeDataClients` and `getClientNames` data format obtained from Pi-hole
  */
 export type IGetClientsOvertimeResponseData = IClientNames & IClientsOvertime;
+
+export type IGetClientsOvertimeFormatted = ChartData<'bar', number[], number>;
 
 /**
  * GET endpoint for `/api/queries/clients/overtime`
@@ -50,6 +66,75 @@ const handleGetClientsOvertimeRaw = (
 };
 
 /**
+ * GET endpoint for `/api/queries/clients/overtime`
+ *
+ * returns formatted data for ChartJS and react-chartjs-2
+ *
+ * @see {@link https://github.com/pi-hole/AdminLTE/blob/master/scripts/pi-hole/js/index.js#L330-L417 | Code inspired by Pi-hole Admin portal}
+ *
+ * @param req - HTTP request provided by NextJS
+ * @param res - HTTP response provided by NextJS
+ */
+const handleGetClientsOvertimeFormatted = (
+  req: NextApiRequest,
+  res: NextApiResponse<IGetClientsOvertimeFormatted | ErrorMessage>,
+) => {
+  const getLogger = logger.scope(apiUrl, 'GET');
+  const { ipAddress, port, password } = req.session.authSession;
+
+  const requestUrl = `http://${ipAddress}:${port}/${clientOvertimeAndNamesUrl()}&auth=${password}`;
+  const responseData: IGetClientsOvertimeFormatted = {
+    labels: [], // unix time
+    datasets: [],
+  };
+
+  axios
+    .get<IGetClientsOvertimeResponseData>(requestUrl)
+    .then((response) => {
+      getLogger.info('data obtained from upstream');
+
+      const timestamps = Object.keys(response.data.over_time);
+      const plotData = Object.values(response.data.over_time);
+      const labels: string[] = [];
+
+      // add client names as labels
+      response.data.clients.forEach((client) => {
+        const clientName = client.name.length > 0 ? client.name : client.ip;
+        labels.push(clientName);
+      });
+
+      for (let i = 0; i < plotData[0].length; i += 1) {
+        responseData.datasets.push({
+          data: [],
+          // If we ran out of colors, make a random one
+          backgroundColor:
+            i < chartThemeColours.length
+              ? chartThemeColours[i]
+              : `#${(0x1000000 + Math.random() * 0xffffff).toString(16).slice(0, 6)}`,
+          label: labels[i],
+        });
+      }
+
+      // Add data for each dataset that is available
+      timestamps.forEach((e, j) => {
+        plotData[j].forEach((element, key) => {
+          responseData.datasets[key].data.push(element);
+        });
+
+        const date = new Date(1000 * parseInt(timestamps[j], 10));
+        responseData.labels?.push(date.getTime());
+      });
+
+      res.status(200).json(responseData);
+      getLogger.complete(`sending response`);
+    })
+    .catch((error) => {
+      getLogger.error(`error returned when sending HTTP request to '${requestUrl}'`);
+      res.status(500).json({ message: JSON.stringify(error) });
+    });
+};
+
+/**
  * Default method to run when executing this http api endpoint
  *
  * @remarks
@@ -64,7 +149,13 @@ const requestHandler = (req: NextApiRequest, res: NextApiResponse) => {
   // limit which HTTP methods are allowed
   switch (method) {
     case 'GET': {
-      handleGetClientsOvertimeRaw(req, res);
+      const { formatted = 'false' } = req.query as IGetRequestData;
+
+      if (formatted === 'true') {
+        handleGetClientsOvertimeFormatted(req, res);
+      } else {
+        handleGetClientsOvertimeRaw(req, res);
+      }
       break;
     }
     default: {
