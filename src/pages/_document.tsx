b9baa06f6164e6ef58cc28f82a5e1b97748fda56
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-30 10:26:14 -0600

    fix(page): load favicon when running NextJS in production mode
      ## what
      - fix load favicon filepath when running NextJS in production mode
    
      ## how
      - change favicon filepath
      - read article
        - https://www.codeconcisely.com/posts/nextjs-favicon/#setting-up-favicon-in-nextjs
    
      ## why
    
      ## where
      - ./src/pages/_document.tsx
    
      ## usage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,7 @@ export default class MyDocument extends Document<MyDocumentProps> {
         <Head>
           {/* PWA primary color */}
           <meta name='theme-color' content={muiDarkTheme.palette.primary.main} />
-          <link rel='shortcut icon' href='/static/favicon.ico' />
+          <link rel='shortcut icon' href='/favicon.ico' />
           <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
