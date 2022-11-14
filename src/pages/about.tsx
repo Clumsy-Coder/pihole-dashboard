4c44a3febe1705bac67f1a0fc6fb115f57334462
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-11-13 17:26:12 -0700

    feat(page): add `About` page
      ## what
      - add about page
      - display
        - Build version. Ex: 1.0.0
        - Build ID: 72f80b3
    
      ## how
      - read build version from `.env.local`
      - read build ID from `.env.local`
    
      ## why
      - this page will display the version and build ID
      - this could be used for debug purposes
    
      ## where
      - ./src/pages/about.tsx
    
      ## usage

---
+++ src/pages/about.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const About: NextPage = () => {
+  return (
+    <Box sx={{ paddingLeft: '5vw' }}>
+      <Head>
+        <title>Pi-hole - About</title>
+        <meta name='description' content='Pi-hole About page' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant='h1'>About</Typography>
+        </Grid>
+        <Grid>
+          <br />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h4' color='textSecondary'>
+            Build version: {process.env.NEXT_PUBLIC_BUILD_VERSION}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h4' color='textSecondary'>
+            Build ID: {process.env.NEXT_PUBLIC_BUILD_ID}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default About;
