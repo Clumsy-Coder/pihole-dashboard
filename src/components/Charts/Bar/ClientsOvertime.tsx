e14aa18766186873ac28586e45a6dc39162654d2
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-08-20 12:02:02 -0600

    fix(component): tooltip clipping for ClientsOvertime bar chart
      ## problem
      - tooltip for ClientsOvertime bar chart is being clipped
    
      ## cause
      - chart is being render via canvas
        - this limits the amount of clients in the tooltip to be displayed,
          causing the tooltip to be clipped
    
      ## solution
      - make a custom render method to display the tooltip on hover
        - code obtained from
          - https://github.com/pi-hole/AdminLTE/blob/41682f17b72d3fb83837a7a08fa68b3e37cd35b7/scripts/pi-hole/js/index.js#L33-L214
        - update chart config to use the custom tooltip
    
      ## where
      - ./src/components/Charts/Bar/ClientsOvertime.tsx
    
      ## usage
    
      ## issue
      resolves #546

--- src/components/Charts/Bar/ClientsOvertime.tsx
+++ src/components/Charts/Bar/ClientsOvertime.tsx
@@ -53,11 +53,16 @@ const padNumber = (num: number) => {
 const options: ChartOptions<'bar'> = {
   maintainAspectRatio: false,
   responsive: true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
   plugins: {
     tooltip: {
-      enabled: true,
-      intersect: false,
-      mode: 'x',
+      enabled: false,
+      position: 'nearest',
+      // intersect: false,
+      // mode: 'x',
       // yAlign: 'bottom',
       xAlign: 'right',
       itemSort: (a: TooltipItem<'bar'>, b: TooltipItem<'bar'>) => {
@@ -74,6 +79,148 @@ const options: ChartOptions<'bar'> = {
           const to = `${padNumber(h)}:${padNumber(m + 4)}:59`;
           return `Client from ${from} to ${to}`;
         },
+      },
+      external: (context) => {
+        // code used to render tooltip differently.
+        // by default the tooltip was rendered in HTML canvas.
+        // since this chart has lots of users for on stacked chart element, the tooltip gets cutoff.
+        // So the tooltip needs to be rendered differently. In this case, using custom tooltip
+        // code obtained from
+        // https://github.com/pi-hole/AdminLTE/blob/41682f17b72d3fb83837a7a08fa68b3e37cd35b7/scripts/pi-hole/js/index.js#L33-L214
+
+        // Tooltip Element
+        const { chart, tooltip } = context;
+        let tooltipEl = chart.canvas.parentNode?.querySelector('div');
+        // console.log(tooltip.options);
+
+        // adding type because `tooltip.options` doesn't seem to have a type for
+        // tooltip.options.bodyFont.family and tooltip.options.bodyFont.style.
+        interface ITooltip {
+          options: {
+            bodyFont: {
+              family: string;
+              style: string;
+            };
+          };
+        }
+
+        if (!tooltipEl) {
+          tooltipEl = document.createElement('div');
+          tooltipEl.id = 'clients-overtime-customTooltip';
+          // console.log('tooltip ID', tooltipEl.id);
+          tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
+          tooltipEl.style.borderRadius = `${tooltip.options.cornerRadius as number}px`;
+          // tooltipEl.style.color = 'white';
+          // tooltipEl.style.opacity = 1;
+          tooltipEl.style.padding = `${tooltip.options.padding as string}px `;
+          tooltipEl.style.pointerEvents = 'none';
+          tooltipEl.style.position = 'absolute';
+          tooltipEl.style.transform = 'translate(-50%, 0)';
+          tooltipEl.style.transition = 'all .1s ease';
+          // tooltipEl.style.font = tooltip.options.bodyFont.string;
+          tooltipEl.style.fontFamily = (tooltip as ITooltip).options.bodyFont.family;
+          tooltipEl.style.fontStyle = (tooltip as ITooltip).options.bodyFont.style;
+          // tooltipEl.style.fontSize = '0.8125rem'
+          tooltipEl.style.fontSize = '12px';
+          // this is needed because the tooltip is showing under the pie chart
+          tooltipEl.style.zIndex = '1';
+          // console.log(tooltip.options.bodyFont);
+
+          const table = document.createElement('table');
+          // table.style.margin = '0px';
+
+          tooltipEl.appendChild(table);
+          chart.canvas.parentNode?.appendChild(tooltipEl);
+        }
+
+        // Hide if no tooltip
+        if (tooltip.opacity === 0) {
+          tooltipEl.style.opacity = `0`;
+          return;
+        }
+
+        // Set Text
+        if (tooltip.body) {
+          const titleLines = tooltip.title || [];
+          const bodyLines = tooltip.body.map((b) => b.lines);
+
+          const tableHead = document.createElement('thead');
+
+          titleLines.forEach((title) => {
+            const tr = document.createElement('tr');
+            tr.style.borderWidth = `0`;
+
+            const th = document.createElement('th');
+            th.style.borderWidth = `0`;
+            const text = document.createTextNode(title);
+
+            th.appendChild(text);
+            tr.appendChild(th);
+            tableHead.appendChild(tr);
+          });
+
+          const tableBody = document.createElement('tbody');
+          // tableBody.style.margin = '10px';
+          // tableBody.style.padding = '10px';
+          bodyLines.forEach((tbody, i) => {
+            const colors = tooltip.labelColors[i];
+
+            const span = document.createElement('span');
+            span.style.background = colors.backgroundColor as string;
+            span.style.borderColor = colors.borderColor as string;
+            span.style.outlineStyle = 'solid';
+            span.style.outlineWidth = '1px';
+            span.style.outlineColor = colors.backgroundColor as string;
+            span.style.borderStyle = 'solid';
+            span.style.borderWidth = '1px';
+            span.style.borderColor = '#fff';
+            span.style.marginRight = '10px';
+            span.style.height = '10px';
+            span.style.width = '10px';
+            span.style.display = 'inline-block';
+
+            const tr = document.createElement('tr');
+            // tr.style.backgroundColor = 'inherit';
+            // tr.style.borderWidth = `0`;
+
+            const td = document.createElement('td');
+            // td.style.borderWidth = `0`;
+
+            const text = document.createTextNode(tbody.join(' '));
+
+            td.appendChild(span);
+            td.appendChild(text);
+            td.style.padding = '0px';
+            tr.appendChild(td);
+            // tr.style.margin = '0px';
+            // tr.style.padding = '0px';
+            tableBody.appendChild(tr);
+          });
+
+          const tableRoot = tooltipEl.querySelector('table');
+
+          // Remove old children
+          while (tableRoot?.firstChild) {
+            tableRoot.firstChild.remove();
+          }
+
+          // Add new children
+          tableRoot?.appendChild(tableHead);
+          tableRoot?.appendChild(tableBody);
+        }
+
+        const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
+
+        // Display, position, and set styles for font
+        console.log(tooltip.options.padding)
+        tooltipEl.style.opacity = `1`;
+        tooltipEl.style.left = `${positionX + tooltip.caretX}px`;
+        tooltipEl.style.top = `${positionY + tooltip.caretY}px`;
+        // tooltipEl.style.font = tooltip.options.bodyFont.string;
+        tooltipEl.style.fontFamily = (tooltip as ITooltip).options.bodyFont.family;
+        tooltipEl.style.fontStyle = (tooltip as ITooltip).options.bodyFont.style;
+        // tooltipEl.style.padding = `${tooltip.options.padding}px ${tooltip.options.padding}px`;
+        tooltipEl.style.padding = `${tooltip.options.padding as string}px`;
       },
     },
   },
