12b816740c355141c60380ece0dbe824043b85c6
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-11-09 12:09:14 -0700

    chore: apply fix for eslint `@typescript-eslint/no-duplicate-type-constituents`
      ## what
      - remove duplicate types for IronSession
    
      ## how
    
      ## why
      - eslint is throwning an error for using union type of duplicates
    
      ## where
      - ./src/lib/AuthSession/Config.ts
    
      ## usage

--- src/lib/AuthSession/Config.ts
+++ src/lib/AuthSession/Config.ts
@@ -1,5 +1,5 @@
 import type { IronSessionOptions } from 'iron-session';
-import type { IronSessionOptions as IronSessionOptionsEdge } from 'iron-session/edge';
+// import type { IronSessionOptions as IronSessionOptionsEdge } from 'iron-session/edge';
 
 /**
  * Time the cookie will be valid for in seconds.
@@ -9,7 +9,7 @@ export const ironSessionTTL = 30 * 60;
 /**
  * Iron session configs
  */
-export const sessionOptions: IronSessionOptions | IronSessionOptionsEdge = {
+export const sessionOptions: IronSessionOptions = {
   // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
   password: process.env.SECRET_COOKIE_PASSWORD,
   cookieName: 'iron-session/pihole/auth',
