41df11aa6bd8abc32fa513b4a9fd38848496e337
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-11-24 19:55:36 -0700

    feat(component): use next-auth `signOut` in authDrawerItems
      ## what
      - use next-auth `signOut` in authDrawerItems
    
      ## how
      - use `signOut` from next-auth
      - notify redux RTK query to invalidate cache for session
        - will cause it to refetch session from the api
      - navigate to login page
    
      ## why
      - this will be used to sign out of the application
      - next-auth will replace iron-session
    
      ## where
      - ./src/components/Drawer/authDrawerItems.tsx
    
      ## usage

--- src/components/Drawer/authDrawerItems.tsx
+++ src/components/Drawer/authDrawerItems.tsx
@@ -2,25 +2,43 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import { useRouter } from 'next/router';
+import { signOut } from 'next-auth/react';
 
 import Link from '@components/Link';
 import { IpageLinks, pageLinks } from '@components/Drawer/pageLinks';
 import { useDeleteAuthSessionMutation } from '@redux/AuthSession';
+import { useAppDispatch } from '@redux/store';
+import api, { TagTypes } from '@redux/apiSlice';
 
 /**
  * Render Drawer list items IF the user is authenticated
  */
 const AuthDrawerListItems: React.FC = () => {
   const { home, logout } = pageLinks;
-  const [deleteAuthSession] = useDeleteAuthSessionMutation();
+  // const [deleteAuthSession] = useDeleteAuthSessionMutation();
   const router = useRouter();
+  const dispatch = useAppDispatch();
 
   const drawerLinks: IpageLinks[] = [home];
 
   const handleLogoutOnclick = () => {
-    deleteAuthSession()
-      .then(() => {
-        router.push('/login').catch(console.error);
+    // deleteAuthSession()
+    //   .then(() => {
+    //     router.push('/login').catch(console.error);
+    //   })
+    //   .catch(console.error);
+
+    signOut({ redirect: false })
+      .then(async () => {
+        // this is needed to update the RTK query cache when logging out.
+        // since logging out is now handled by NextAuth, RTK query needs to invalidate the cached tag `AUTH`.
+        // This will cause RTK query hook for getting session to refetch from the api
+        //
+        // obtained from
+        // https://redux-toolkit.js.org/rtk-query/usage/manual-cache-updates#general-updates
+        dispatch(api.util.invalidateTags([TagTypes.AUTH]));
+
+        await router.push('/login');
       })
       .catch(console.error);
   };
