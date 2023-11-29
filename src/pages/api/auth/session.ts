a768443dbafa46e7b9eac3d68515554afd236405
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-11-29 09:03:16 -0700

    refactor(api): use NextAuth get server session
      ## what
      - use NextAuth get server session
    
      ## how
      - use NextAuth function `getServerSession` to get session
        - this will return `ipAddress` and `port`
    
      ## why
      - this will replace `iron-session` for getting session cookie
    
      ## where
      - ./src/pages/api/auth/session.ts
    
      ## usage

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -1,7 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+// import { getToken } from 'next-auth/jwt';
 
-import { IAuthSession, withSessionRoute } from '@lib/AuthSession';
+import {
+  IAuthSession,
+  // withSessionRoute
+} from '@lib/AuthSession';
 import logger from '@utils/logger';
+import { authOptions } from '@pages/api/auth/[...nextauth]';
+import { IUser, ISession } from '@lib/types/next-auth';
 
 /**
  * Get Login session.
@@ -11,7 +18,7 @@ import logger from '@utils/logger';
  * @param req - HTTP request provided by NextJS
  * @param res - HTTP response provided by NextJS
  */
-const handleGet = (req: NextApiRequest, res: NextApiResponse<IAuthSession>) => {
+const handleGet = async (req: NextApiRequest, res: NextApiResponse<IAuthSession>) => {
   const getLogger = logger.scope('/api/auth/session', 'GET');
 
   getLogger.info(`checking is auth session exists`);
@@ -22,16 +29,40 @@ const handleGet = (req: NextApiRequest, res: NextApiResponse<IAuthSession>) => {
     password: '',
   };
 
-  if (req.session.authSession) {
-    const { ipAddress, port } = req.session.authSession;
+  // if (req.session.authSession) {
+  //   const { ipAddress, port } = req.session.authSession;
+  //
+  //   response.ipAddress = ipAddress;
+  //   response.port = port;
+  //
+  //   getLogger.info(`auth session found`);
+  // } else {
+  //   getLogger.info(`auth session NOT found`);
+  // }
 
+  const session: ISession | null = await getServerSession(req, res, authOptions);
+  // const jwt = await getToken({ req });
+
+  // if user is not authenticated
+  if (!session || !session.user) {
+    getLogger.info(`auth session NOT found`);
+    // res.status(200).json(response);
+    // return;
+  }
+  // if user is authenticated
+  else {
+    getLogger.info('session: ', session);
+    const { ipAddress, port } = session.user as IUser;
     response.ipAddress = ipAddress;
     response.port = port;
-
-    getLogger.info(`auth session found`);
-  } else {
-    getLogger.info(`auth session NOT found`);
   }
+
+  // if (!jwt) {
+  //   res.status(200).json(response);
+  //   return;
+  // }
+
+  // getLogger.debug('jwt: ', jwt);
 
   getLogger.complete(`sending response`);
   getLogger.debug(`response data`, response);
@@ -48,13 +79,13 @@ const handleGet = (req: NextApiRequest, res: NextApiResponse<IAuthSession>) => {
  * @remarks
  * HTTP method allowed: `GET`
  */
-const mainHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const mainHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method = '' } = req;
 
   // limit which HTTP methods are allowed
   switch (method) {
     case 'GET': {
-      handleGet(req, res);
+      await handleGet(req, res);
       break;
     }
     default: {
@@ -68,4 +99,5 @@ const mainHandler = (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export default withSessionRoute(mainHandler);
+// export default withSessionRoute(mainHandler);
+export default mainHandler;
